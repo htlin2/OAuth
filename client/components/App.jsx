839aa7e3e944877f885bd0f8410b77cc36453ee5
htlin2 <htlin2@gmail.com>
2019-02-06 20:03:01 -0800

    front: added form submission

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,20 +5,42 @@ class App extends React.Component {
     super(props);
     this.state = {
       data: [],
+      enteredItem: '',
     }
+
+    this.getTodos = this.getTodos.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleItemInput = this.handleItemInput.bind(this);
   }
 
   componentDidMount() {
+    this.getTodos();
+  }
+
+  getTodos(userId = 1) {
+    fetch(`/todos/${userId}`)
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(this.state);
+  }
+
+  handleItemInput(e) {
+    this.setState({ enteredItem: e.target.value });
   }
 
   render() {
     return (
       <div className="container">
         <div className="containerLeft">
-          <h2>Left</h2>
-        </div>
-        <div className="containerRight">
-          <h2>Right</h2>
+          <h2>Todos</h2>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text" value={this.state.enteredItem} onChange={this.handleItemInput} />
+            <input type="submit" />
+          </form>
         </div>
       </div>
     )
